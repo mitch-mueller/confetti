3fdd5eed9d72de51f30c25c7c6fd746513613912
Michael Müller <muellermj97@gmail.com>
2017-12-30 23:58:17 +0100

    added jQuery plugin basic function

--- dist/confetti.js
+++ dist/confetti.js
@@ -1,3 +1,25 @@
 /*
 confetti.js by mitch-mueller
 */
+(function ($) {
+    $.fn.confetti = function( options ) {
+
+        //default options
+        //creating an object and adding default, then user options
+        var opts = $.extend({}, $.fn.confetti.defaults, options);
+
+        return this;
+
+        /*
+        if selector targets more than one element, each() is required
+
+        return this.each(function() {
+
+        });
+        */
+    };
+
+    $.fn.confetti.defaults = {
+
+    };
+}(jQuery));
