382aefd2100cbb34fc8916ad0a8e28cdd6f310f2
Michael Müller <muellermj97@gmail.com>
2017-12-31 02:45:09 +0100

    added more comments to code, also fixed an absolute value in code

--- dist/confetti.js
+++ dist/confetti.js
@@ -4,70 +4,83 @@ confetti.js by mitch-mueller
 (function ($) {
     $.fn.confetti = function( options ) {
 
+        // figure out dimensions of canvas
         var targetWidth = $(window).innerWidth();
         var targetHeight = $(window).innerHeight();
 
-        var canvas = $("<canvas/>")
-            .prop({
+        var canvas = $("<canvas/>") // canvas creation
+            .prop({ // dimensions of canvas
                 width: targetWidth,
                 height: targetHeight
             })
-            .css({
+            .css({ // position of canvas
                 "position": "fixed",
                 "z-index": "10000000",
                 "top": "0",
                 "left": "0"
             })
-            .appendTo(this)
-            .get(0);
+            .appendTo(this) // add to body
+            .get(0); // make it a canvas that native javascript can handle
 
-        var context = canvas.getContext("2d");
+        var context = canvas.getContext("2d"); // create context to draw on
 
         //default options
         //creating an object and adding default, then user options
         var opts = $.extend({}, $.fn.confetti.defaults, options);
 
+        // used for logging
         var stats = {
             frames: 0,
             totalFlakesGenerated: 0,
             lastTotalFlakesGenerated: 0
         }
 
+        // holds all flakes that are rendered
         var flakes = [];
 
-        //TODO ::: 1920
+        /*
+         *  @param i
+         *  creates a flake at index i
+         *  using random variations based on options
+         */
         var flakeFactory = function(i) {
             flakes[i] = {
-                "x": Math.floor((Math.random() * (1920-opts.maxSize)) + opts.maxSize),
+                "x": Math.floor((Math.random() * (targetWidth-opts.maxSize)) + opts.maxSize),
                 "y": 0,
                 "size": Math.floor((Math.random() * opts.maxSize) + opts.minSize),
                 "speed": Math.floor((Math.random() * opts.maxSpeed) + opts.minSpeed),
                 "color": opts.colors[Math.floor(Math.random() * opts.colors.length)]
             };
-            stats.totalFlakesGenerated++;
+            stats.totalFlakesGenerated++; // again, just for logging purposes
         }
 
+        // first run, please create flakes
         for (var i = 0; i<opts.population; i++) {
             flakeFactory(i);
         }
 
+        // loop that executes n-times a second
         var loop = function(context, flakes) {
             logic(flakes);
             render(context, flakes);
         }
 
+        //figure out location of each flake
         var logic = function(flakes) {
 
-            if (opts.population > flakes.length) {
+            
+            if (flakes.length < opts.population) // not enough flakes on screen
                 for (var i = flakes.length; i<opts.population; i++) {
-                    flakeFactory(i);
+                    flakeFactory(i); // create new flake
                 }
-            } else if (opts.population < flakes.length) {
+            else if (opts.population < flakes.length) // too many flakes on screen
                 for (var i = opts.population; i<flakes.length; i++) {
-                    flakes.splice(i, 1);
+                    flakes.splice(i, 1); // remove flake
                 }
-            }
+            
 
+            // for every flake, move according to individual speed.
+            // if out of screen, replace with new flake starting from the top    
             for (var i = 0; i<flakes.length; i++) {
                 flakes[i].y += flakes[i].speed;
                 if (flakes[i].y > 1080) {
@@ -88,9 +101,13 @@ confetti.js by mitch-mueller
 
         };
 
+        // render all flakes on screen
         var render = function(context, flakes) {
-            context.clearRect(0, 0, 1920, 1080);
 
+            // clear the screen
+            context.clearRect(0, 0, 1920, 1080); 
+
+            // for every flake, draw a circle based on individual attributes
             for (var i = 0; i < flakes.length; i++) {
 
                 var flake = flakes[i];
@@ -100,23 +117,19 @@ confetti.js by mitch-mueller
                 context.fillStyle = flake.color;
                 context.fill();
             }
+
+            // again, just for statistics
             stats.frames++;
         };
 
+        // repeat loop() a frameRate-times a second
         window.setInterval(function() {loop(context, flakes);}, 1000/opts.frameRate);
 
-
+        // yeah, chaining is awesome B-)
         return this;
-
-        /*
-        if selector targets more than one element, each() is required
-
-        return this.each(function() {
-
-        });
-        */
     };
 
+    // default values, can be overriden by passing an {} to confetti()
     $.fn.confetti.defaults = {
         population: 80,
         minSize: 16,
